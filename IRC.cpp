90c4cf77ab9fd04914150144413268e0880713ef
Julien QUIL <jquil@bess-f2r2s9.clusters.42paris.fr>
2024-04-22 14:17:42 +0200

    ok

--- IRC.cpp
+++ IRC.cpp
@@ -6,7 +6,7 @@
 /*   By: jquil <jquil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 18:07:38 by jquil             #+#    #+#             */
-/*   Updated: 2024/04/19 18:00:12 by jquil            ###   ########.fr       */
+/*   Updated: 2024/04/22 14:17:18 by jquil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,23 @@
 IRC::IRC(int port, std::string mdp)
 {
 	std::cout << "Default IRC constructor" << std::endl;
-
-	std::cout << "" << std::endl;
-
-
-	// Init server name to 0000
 	memset(&this->server, 0, sizeof(this->server));
-
-	// Définit l'ecoute sur toutes les interfaces
 	this->server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);// 127.0.0.1, localhost
-
-	// Port sur lequel doit écouter le serveur
 	this->server.sin_port = htons(port);
-
-	// Compteur de poll (nombre de socket à surveiller)
 	this->poll_count = 1;
-
-	// Boolen de secu pour poll
 	this->poll_size = 2;
 	this->secure = 0;
-
-	// Init tab poll à NULL
 	this->poll_fds = NULL;
-
-	// Type de protocole d'écoute internet
 	this->server.sin_family = AF_INET;
-
-	// FAMILLE :AF_INET pour utiliser protocole TCP/IP avec adresse de plus de 4 octets
-	// TYPE : SOCK_STREAM pour mode connecté (connexion permanente)
-	// PROTOCOL : IPPROTO_TCP pour TCP
 	this->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	// Test de la socket
 	if (this->calloc_pollfd(10) == 0)
 	this->secure = 1;
 	this->poll_fds[0].fd = this->sock;
 	this->poll_fds[0].events = POLLIN;
-	//std::signal(SIGINT, closeServer);
-	//std::signal(SIGQUIT, closeServer);
 	if (this->sock < 0)
 		this->secure = 1;
 	else
-		this->bind_sock = bind(this->sock, (struct sockaddr *)&this->server, sizeof(this->server)); // invalid argument -> errno 99
+		this->bind_sock = bind(this->sock, (struct sockaddr *)&this->server, sizeof(this->server));
 	if (this->bind_sock < 0)
 	{
 		this->secure = 1;
@@ -67,10 +42,7 @@ IRC::IRC(int port, std::string mdp)
 	if (this->lstn < 0)
 		this->secure = 1;
 	this->peer_addr_size = sizeof (struct sockaddr_in);
-	// Def du port
 	this->port = port;
-
-	// Def du mdp
 	this->mdp = mdp;
 };
 
@@ -120,63 +92,34 @@ void	IRC::launch_serv(void)
 		std::cout << "Initialisation failure, exit the program" << std::endl;
 		return ;
 	}
-
-	// Server buffer ppour les messages
 	char server_recv[200];
-
 	std::cout << "Server launched, listening on port : " << this->port << std::endl;
 	while (42)
 	{
-		// Lancement de poll ()
 		int status = poll(this->poll_fds, this->poll_count, 9000);
-
- 		// Security check du lancement de poll
 		if (status == -1)
 		{
 			std::cout << "Poll failure" << std::endl;
 			return ;
 		}
-
-		// Routine
 		for (int x = 0; x < 10; x++)
 		{
-
 			if (this->poll_fds[x].fd == this->sock)
 			{
- 				// Accept/Parse de la connexion entrante (Socket ouverte, addr du nouveau client, taille de l'addr)
-				// Renvoie un descripteur de fichier pour la socket client
 				int client = accept(this->sock, (struct sockaddr *)&this->peer_addr, &this->peer_addr_size);
-
-
 				if (client > 0)
 				{
 					if (this->add_poll_fds(client) == 0)
 						return ;
- 					// Lit dans la socket client (socket client, buff, nb octets à lire, flags)
 					recv(client, server_recv, 200, 0);
-
-					// Créé le nv client
 					class client cl(client, server_recv, this->mdp);
-
-					// Check mdp renseigné par le client
 					if (check_pass(cl) == 1)
 					{
-						// Enregistre le client dans la map
 						this->users[client] = cl;
-
-						// Connect le nouveau client
 						connect(client, (struct sockaddr *)&this->peer_addr, this->peer_addr_size);
-
-						// Créé et envoie un message de bienvenue
 						std::string acc = ":localhost 001 " + cl.GetNick() + " :Welcome to bdtServer " + cl.GetNick() + "!~" + cl.GetUser() + "@127.0.0.1\r\n";
 						std::cout << acc << std::endl;
-
-						// Envoie le message de bienvenue dans la socket du client
 						send(client, acc.c_str(), 512, 1);
-						std::cout << "\n\n\n" << server_recv << std::endl;
-						void *buf = NULL;
-						read(client, buf, 100);
-						std::cout << buf << std::endl;
 					}
 					else
 						std::cout << "Wrong password" << std::endl;
