fb40b40757c91958cb4f18f77db9bf6656d880a8
Lucas BOUGUET <lbouguet@bess-f4r4s5.clusters.42paris.fr>
2024-04-23 10:23:11 +0200

    Set topic done

--- topic.cpp
+++ topic.cpp
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   topic.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jquil <jquil@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lbouguet <lbouguet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:15:23 by jquil             #+#    #+#             */
-/*   Updated: 2024/04/22 18:15:31 by jquil            ###   ########.fr       */
+/*   Updated: 2024/04/23 10:22:11 by lbouguet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "IRC.hpp"
+
+bool						IRC::topic(client &client, std::string cmd)
+{
+	for (std::vector<Channel>::iterator it = this->channels.begin();
+		it != this->channels.end(); it++)
+	{
+		for (size_t i = 0; i != it->getTopicOperators().size() ; i++)
+		{
+			if (it->getTopicOperators()[i] == client.GetSock())
+				it->setTopic(cmd);
+		}
+	}
+}	
